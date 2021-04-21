92129e21a17c7389b08c71581b3e347d3304ff42
RealDebugg <52179011+RealDebugg@users.noreply.github.com>
2021-04-21 20:15:49 +0200

    Update client.lua

--- debugg-actiontoggle/client.lua
+++ debugg-actiontoggle/client.lua
@@ -1,11 +1,10 @@
 local isAllowed = false
-Controlkey = {["toggleActionMode"] = {20,"Z"}} 
 CreateThread(function()
     while true do
         Wait(0)
         if IsPedUsingActionMode(GetPlayerPed()) and not isAllowed then
             SetPedUsingActionMode(GetPlayerPed(), 0, -1, 0)
-        elseif IsControlJustPressed(0, Controlkey["toggleActionMode"][1]) then --Z
+        elseif IsControlJustPressed(0, 20) then
             isAllowed = not isAllowed
             if isAllowed and not IsPedUsingActionMode(GetPlayerPed()) then
                 SetPedUsingActionMode(GetPlayerPed(), 1, -1, 0)
