fdc89b00e98704bb5bd8abf053d176a53754039c
RealDebugg <52179011+RealDebugg@users.noreply.github.com>
2022-08-23 22:13:00 +0200

    math functions for js

--- snippets/cl.js
+++ snippets/cl.js
@@ -14,3 +14,35 @@ async function CheckForProfanity(string) {
     }
     return ScProfanityGetStringPassed(token);
 }
+
+var math = { //custom math functions
+    random: function(min, max) {
+        if (max == null || max == undefined) {
+            return Math.floor(Math.random() * min);
+        } else {
+            return Math.floor(Math.random() * (max - min) + min);
+        }
+    },
+    distance: function(cc1, cc2, ignoreZ = false) {
+        if (!ignoreZ) {
+            return Math.hypot(cc1[0] - cc2[0], cc1[1] - cc2[1], cc1[2] - cc2[2])
+        } else {
+            Math.hypot(cc1[0] - cc2[0], cc1[1] - cc2[1])
+        }
+    },
+    Vector3: function(x, y, z) {
+        return [x, y, z]
+    },
+    DateToMinutesAgo: function(date) {
+        return time = ((Date.now() - date) / 60000);
+    },
+    MinutesLeft: function(time, mins) {
+        let timeTot = mins - time;
+        if (timeTot > 0) {
+            timeTot = Math.ceil(timeTot);
+        } else {
+            timeTot = 0;
+        }
+        return timeTot
+    }
+}
